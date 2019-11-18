3d11729729727d4d79f5d058e67bebe688256018
Clara Hultén <clahul-9@student.ltu.se>
2019-11-18 20:48:14 +0100

    3

---
+++ Print_2D_array_with_rowsColumns/Print_2D_array_with_rowsColumns/print_2d_array.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include <cstdlib>
+
+using namespace std;
+int array_rows_cols(int arr[][]) {
+
+}
+
+
+int main() {
+	int r;
+	for (int i = 0; i < 4; i++)
+	{
+		r = rand() % 10;
+		cout << r << endl;
+	}
+	system("pause");
+}
---
+++ Swap_sort/Swap_sort/swapSort cpp.cpp
@@ -0,0 +1,69 @@
+#include<iostream>
+#include<stdio.h>
+
+using namespace std;
+void swap_sort(int a,int b,int c,bool order){
+	int n[] = {a,b,c};
+	cout << order << endl;
+	for (int j = 0; j < 3; j++)
+	{
+		for (int i = 0; i < 3; i++) {
+			if (order == 0 && n[i] >= n[i + 1] && n[i + 1] > 0)
+			{
+				int d = n[i];
+				n[i] = n[i + 1];
+				n[i + 1] = d;
+
+				if (n[i + 1] < 0)
+				{
+					break;
+				}
+			}
+			else if (order == 1 && n[i] <= n[i + 1] && n[i + 1] > 0)
+			{
+				//printf("före n0: %i, n1: %i \n", n[i], n[i + 1]);
+				int d = n[i];
+				n[i] = n[i + 1];
+				n[i + 1] = d;
+				//printf("efter n0: %i, n1: %i \n", n[i], n[i + 1]);
+				//system("pause");
+				if (n[i + 1] < 0)
+				{
+					break;
+				}
+			}
+		}
+	}
+	printf("\nResult: ");
+	for (auto e : n)
+	{
+		cout << e;
+	}
+
+}
+
+int main() {
+	int a=1;
+	do
+	{
+		printf("\nGive a (0 to quit): ");
+		cin >>a;
+		if (a != 0)
+		{
+			printf("Give b: ");
+			int b;
+			cin>>b;
+
+			printf("Give c: ");
+			int c=0;
+			cin >> c;
+
+			printf("Sort ascending/descending (1/0): ");
+			bool o=0;
+			cin >> o;
+
+			swap_sort(a, b, c,o);
+		}
+
+	} while (a!=0);
+}
