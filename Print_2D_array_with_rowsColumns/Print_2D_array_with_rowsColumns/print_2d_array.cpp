8ec203849b6d1f404c889b94def8973b1051ede4
Clara Hultén <clahul-9@student.ltu.se>
2019-11-21 14:31:00 +0100

    5

--- Print_2D_array_with_rowsColumns/Print_2D_array_with_rowsColumns/print_2d_array.cpp
+++ Print_2D_array_with_rowsColumns/Print_2D_array_with_rowsColumns/print_2d_array.cpp
@@ -2,7 +2,41 @@
 #include <cstdlib>
 
 using namespace std;
-int array_rows_cols(int *arr) {
+
+int array_rows_cols(int arr[100][100],int r, int c) {
+	int sumR=0,sumC = 0,tot_sum=0;
+
+	for (int x = 0; x < r; x++)
+	{
+		for (int y = 0; y <= c; y++)
+		{
+			if (y==c)
+			{
+				printf("  %i \n", sumR);
+				tot_sum += sumR;
+				sumR = 0;
+				break;
+			}
+			sumR += arr[x][y];
+			printf("%i  ", arr[x][y]);
+		}
+	}
+	cout << endl;
+	for (int i = 0; i < c; i++)
+	{
+		for ( int j = 0; j <=r; j++)
+		{
+			if (j == r)
+			{
+				printf("%i ", sumC);
+				tot_sum += sumC;
+				sumC = 0;
+				break;
+			}
+			sumC += arr[j][i];
+		}
+	}
+	printf("  %i", tot_sum);
 
 	return 0;
 }
@@ -10,35 +44,27 @@ int array_rows_cols(int *arr) {
 
 int main() {
 	int r, c;
-
 	printf("Number of rows: ");
 	cin >> r;
 
 	printf("Number of columns: ");
 	cin >> c;
 
-	int *arr = new int[r, c];
+	int arr[100][100];
 
-	for (int i = 0; i < c; i++)
+	for (int i = 0; i < r; i++)
 	{
-		for (int j = 0; j < r; j++)
+		for (int j = 0; j < c; j++)
 		{
-			arr[i, j] = rand() % 10;
-			printf("%i ", arr[i, j]);
+			arr[i][j] = rand() % 10;
+			//printf("%i ", arr[i, j]);
 		}
-		printf("\n");
+		//printf("\n");
 	}
-	printf("----------------------------- \n");
-	for (int x = 0; x < c; x++)
-	{
-		for (int y = 0; y < r; y++)
-		{
-			printf("%i  ", arr[x, y]);
-		}
-		cout << "\n";
-	}
-	
+	//printf("----------------------------- \n");
 
+	array_rows_cols(arr,r,c);
+
+	cout << "\n";
 	system("pause");
-	delete arr;
 }
